ab171c4f1fb0d16a1e357e2d522c6cb8c3fc1689
Lupita <lupitarivera8899@gmail.com>
2021-04-29 08:26:45 -0500

    updating users table with provider and uid

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,31 @@
 class UsersController < ApplicationController
 
     def new
+        @user = User.new
 
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:message] = "Succesfully signed up"
+            session[:user_id] = @user.id
+            redirect_to photos_path
+        else 
 
-    def show
-        
+            render :new
+        end
+
+
+    end
+    
+
+    # def show
+
+    # end
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
     end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_29_033840) do
+ActiveRecord::Schema.define(version: 2021_04_29_132248) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 2021_04_29_033840) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "provider"
+    t.string "uid"
   end
 
   add_foreign_key "comments", "photos"

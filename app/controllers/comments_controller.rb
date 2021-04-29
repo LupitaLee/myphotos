502a4b9a69421dc5cb48069bb4470547163b6255
Lupita <lupitarivera8899@gmail.com>
2021-04-28 22:41:01 -0500

    add crud for photos

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,57 @@
 class CommentsController < ApplicationController
+
+    before_action :set_comment, only: [:show, :edit, :update, :create, :destroy]
+
+    def index
+        @comments = Comment.all
+    end
+
+    def new
+        @comment = Comment.new 
+    end
+    def show 
+       
+    end
+    
+    def create
+        
+        if @comment.save
+            redirect_to comment_path
+        else
+            render :new
+        end
+    end
+
+    def edit 
+       
+    end
+
+    def update 
+       
+        @comment.update(comment_params)
+        if @comment.save 
+            redirect_to comment_path
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+     
+        @comment.destroy 
+        flash[:notice]= "Photo deleted"
+        redirect_to comment_path 
+
+    end
+
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content)
+    end
+
+    def set_comment
+        @comment = Comment.find(params[:id])
+    end
 end
--- db/migrate/20210429033840_create_comments.rb
+++ db/migrate/20210429033840_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
       t.string :content
-      t.belongs_to :photos, null: false, foreign_key: true
+      t.belongs_to :photo, null: false, foreign_key: true
       t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps

dc120b70f9fd3d16ab5fd1739775c59cab010380
Lupita <lupitarivera8899@gmail.com>
2021-05-07 11:52:30 -0500

    when signout redirect to root

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 
     def logout
         session.clear
-        redirect_to login_path
+        redirect_to root_path
     end
 
     def omniauth

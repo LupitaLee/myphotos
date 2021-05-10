9ca4d200b00c0633729e6b7391f4af624928887f
Lupita <lupitarivera8899@gmail.com>
2021-05-10 08:39:42 -0500

    remove /account route

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
 
-  get '/account', to:'users#account'
-
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
 
